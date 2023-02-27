4830f5058fdc3cb2afb84175d0f198d843686647
Tiago Guerra <57842819+tiagodsg1@users.noreply.github.com>
2023-02-27 11:31:29 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+import tkinter as tk
+from tkinter import ttk
+
+def teste():
+    janela = tk.Tk()
+    janela.title("Teste")
+    janela.geometry("300x300")
+    janela.mainloop()
+teste()
